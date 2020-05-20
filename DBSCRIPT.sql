3e03b13aeaec6aa510243b4575d98a26ffbf14be
contractcooker <barron.tom@gmail.com>
2020-05-20 10:46:47 -0400

    use plantfinderdb added to dbscript

--- DBSCRIPT.sql
+++ DBSCRIPT.sql
@@ -1,11 +1,15 @@
 USE master
 IF EXISTS(select * from sys.databases where name='PlantFinderDB')
+ALTER DATABASE PlantFinderDB  SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
 DROP DATABASE PlantFinderDB
 GO
 CREATE DATABASE PlantFinderDB
 GO
-
+Use PlantFinderDB
+GO
 DROP TABLE IF EXISTS Users;
+GO
 
 CREATE TABLE Users (
 ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
@@ -15,6 +19,7 @@ Email varchar (500) NOT NULL,
 Fname varchar (80) NOT NULL,
 Lname varchar (80) NOT NULL,
 );
+GO
 
 INSERT INTO users(username, password, email, fname, lname)
 VALUES
@@ -24,14 +29,14 @@ VALUES
 ('MDuBose', 'BestDogMom202', 'iloveriley@gmail.com', 'Michael', 'DuBose')
 
 DROP TABLE IF EXISTS Plants;
-
+GO
 CREATE TABLE Plants (
 ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
 Title varchar (80) NOT NULL,
 Image varchar (500) NOT NULL,
 Description varchar (500) NOT NULL,
 );
-
+GO
 
 INSERT INTO Plants (Title, Image, Description)
 VALUES
@@ -39,16 +44,16 @@ VALUES
 'Besides looking cool, it is a low-maintenance plant that is known for surviving droughts, making it perfect for newbies living in almost any environment. Although the snake plant prefers bright light, it can survive lower light levels.'),
 ('Pothos', 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1551637106-screen-shot-2019-03-03-at-1-16-56-pm-1551637036.png?crop=0.835xw:0.649xh;0.0978xw,0.270xh&resize=768:*', 'Sometimes referred to as the cubicle plant, the pothos is a great pick if you are a self-proclaimed "black thumb".  NOT pet friendly!'),
 ('Dracaena', 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1551637334-41655UkNk2L.jpg?crop=0.928xw:0.928xh;0.0400xw,0.0400xh&resize=768:*', 'If you live a high-maintenance life—always on the go, out at all hours—you need an "easy going, low-maintenance plant, like the Dracaena');
-
+GO
 DROP TABLE IF EXISTS My_Plants;
 
 
-CREATE TABLE My_Plants(
+CREATE TABLE My_Plants (
 ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
 Water_Completed bit,
 UserID int NOT NULL,
 PlantID int NOT NULL
-); 
+);
 INSERT INTO My_Plants (UserID, Water_Completed, PlantID)
 VALUES
 (4, '1', 10),
@@ -57,7 +62,7 @@ VALUES
 DROP TABLE IF EXISTS Wish_List;
 
 
-CREATE TABLE Wish_List(
+CREATE TABLE Wish_List (
 ID int NOT NULL PRIMARY KEY IDENTITY(1,1),
 UserID int NOT NULL,
 PlantID int NOT NULL
